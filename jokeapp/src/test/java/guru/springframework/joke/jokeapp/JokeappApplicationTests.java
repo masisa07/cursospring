0f3a661db34a3f0a28a9b448a1e955c26fff579f
Manuel Sierra Sanz <manuel.sierra@sngular.team>
2018-07-25 14:04:11 +0200

    4.56 Creating the View Layer

--- jokeapp/src/test/java/guru/springframework/joke/jokeapp/JokeappApplicationTests.java
+++ jokeapp/src/test/java/guru/springframework/joke/jokeapp/JokeappApplicationTests.java
@@ -1,6 +1,5 @@
 package guru.springframework.joke.jokeapp;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +7,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Ignore
 public class JokeappApplicationTests {
 
 	@Test
