a361f6876b1567b08db422d61f60e33645ea897a
Manuel Sierra Sanz <manuel.sierra@sngular.team>
2018-08-02 14:06:39 +0200

    8.130 Spring Data JPA Query Methods

--- spring5-recipe-app/src/main/java/guru/springframework/controllers/IndexController.java
+++ spring5-recipe-app/src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,7 +1,14 @@
 package guru.springframework.controllers;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.domain.Category;
+import guru.springframework.domain.UnitOfMeasure;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
 
 /**
  * Created by jt on 6/1/17.
@@ -9,8 +16,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
-    @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        return "index";
-    }
+	private CategoryRepository categoryRepository;
+	private UnitOfMeasureRepository unitOfMeasureRepository;
+
+	public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+		super();
+		this.categoryRepository = categoryRepository;
+		this.unitOfMeasureRepository = unitOfMeasureRepository;
+	}
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+		
+		Optional<Category> categoryOptional = categoryRepository.findByDescription("Americana");
+		Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("kilo");
+		
+		System.out.println("-------------> Cat id: "+ categoryOptional.get().getId());
+		System.out.println("-------------> UOM id: "+ uomOptional.get().getId());
+		
+		return "index";
+	}
 }
